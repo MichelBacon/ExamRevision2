997f431a58b9eea1f1c9cf0c11508653f9c48d15
B533P-10\533 <mickaelbergeron14@gmail.com>
2019-12-03 11:52:55 -0500

    Question 4 fait

--- Revision_finale/Assets/Question4/FishGenerator.cs
+++ Revision_finale/Assets/Question4/FishGenerator.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class FishGenerator : MonoBehaviour {
+public class FishGenerator : MonoBehaviour
+{
 
     public int mapWidth = 256;
+    public int mapHeight = 256;
     public float noiseScale;
     public int octaves;
-    [Range(0,1)]
+    [Range(0, 1)]
     public float persistance;
     public float lacunarity;
     public bool autoUpdate;
@@ -17,37 +19,55 @@ public class FishGenerator : MonoBehaviour {
 
     public int maxNumberOfFishes = 100;
     public GameObject fishPrefab;
+    private void Start()
+    {
 
-    private void Start() {
-       
     }
 
-    public void GenerateFishes() {
+    public void GenerateFishes()
+    {
         DeleteFishes();
-        
-        
+
+        float[,] noiseMapFish = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                if(noiseMapFish[x,y] > 0.8)
+                {
+                    Instantiate(fishPrefab, new Vector3(x, y, 0), new Quaternion(0, 0, 0, 0));
+                }    
+            }
+        }
     }
 
-    public void DeleteFishes() {
-        foreach (Fish fish in FindObjectsOfType<Fish>()) {
+    public void DeleteFishes()
+    {
+        foreach (Fish fish in FindObjectsOfType<Fish>())
+        {
             DestroyImmediate(fish.gameObject);
         }
     }
-    
-  
-    private void OnValidate() {
-        if(mapWidth< 1) {
+
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
             mapWidth = 1;
         }
-        if (lacunarity < 1) {
+        if (lacunarity < 1)
+        {
             lacunarity = 1;
         }
-        if(octaves < 0) {
+        if (octaves < 0)
+        {
             octaves = 0;
         }
 
     }
-    
-    
+
+
 }
 
--- Revision_finale/Assets/Question4/MovementGenerator.cs
+++ Revision_finale/Assets/Question4/MovementGenerator.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class MovementGenerator : MonoBehaviour {
+public class MovementGenerator : MonoBehaviour
+{
 
     public int mapWidth = 256;
+    public int mapHeight = 256;
     public float noiseScale;
     public int octaves;
     [Range(0, 1)]
@@ -21,35 +23,50 @@ public class MovementGenerator : MonoBehaviour {
 
     private float[,] noiseMapX;
     private float[,] noiseMapY;
-    private void Start() {
-        
+    private void Start()
+    {
+        noiseMapX = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+        noiseMapY = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed + 1, noiseScale, octaves, persistance, lacunarity, offset);
         timeLastUpdate = Time.time;
     }
 
-    private void Update() {
-        foreach (Fish fish in FindObjectsOfType<Fish>()) {
+    private void Update()
+    {
+        foreach (Fish fish in FindObjectsOfType<Fish>())
+        {
             //Code pour le mouvement des poissons
-           
-
+            Rigidbody2D rigidbody2D = fish.GetComponent<Rigidbody2D>();
+            int x = (int)fish.transform.position.x % mapWidth;
+            int y = (int)fish.transform.position.y % mapHeight;
+            x = Math.Abs(x);
+            y = Math.Abs(y);
+            rigidbody2D.AddForce(new Vector2((noiseMapX[x, y] - 0.5f), (noiseMapY[x, y] - 0.5f)));
         }
-        if (Time.time - timeLastUpdate > 5) {
+        if (Time.time - timeLastUpdate > 5)
+        {
             //Code pour nouveau mouvement
-           
+
+            noiseMapX = Noise.GenerateNoiseMap(mapWidth, mapHeight, UnityEngine.Random.Range(1, 25), noiseScale, octaves, persistance, lacunarity, offset);
+            noiseMapY = Noise.GenerateNoiseMap(mapWidth, mapHeight, UnityEngine.Random.Range(1, 25), noiseScale, octaves, persistance, lacunarity, offset);
             timeLastUpdate = Time.time;
         }
-       
+
     }
-    
 
 
-    private void OnValidate() {
-        if (mapWidth < 1) {
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
             mapWidth = 1;
         }
-        if (lacunarity < 1) {
+        if (lacunarity < 1)
+        {
             lacunarity = 1;
         }
-        if (octaves < 0) {
+        if (octaves < 0)
+        {
             octaves = 0;
         }
 
